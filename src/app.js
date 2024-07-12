d6ae38fbdfed1f15cf57e914f787b5edf2a36800
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2024-07-12 07:27:07 +0500

    add

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use("/auth", authRoute);
 app.use("/category", categoryRoute);
 app.use("/product", productRoute);
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 })
